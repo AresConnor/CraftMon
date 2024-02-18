525433e0ce963db35c18c343dd85c02921485a1c
AresConnor <39639716+AresConnor@users.noreply.github.com>
2024-02-18 14:04:28 +0800

    init

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import os
-import yaml
-import re
 import requests
+import yaml
 from flask import Flask, render_template, send_from_directory
-from utils.mcclient import QueryClient
+
+from utils.sponsors import load_sponsors
 
 app = Flask(__name__)
 app.template_folder = 'templates'
@@ -20,71 +19,67 @@ mc_logo = conf['server']['logo']
 mc_preview_title = conf['server']['preview']['title']
 mc_preview_descr = conf['server']['preview']['descr']
 mc_preview_images = conf['server']['preview']['images']
-join_content = conf['server']['contact']['content']
+
+sponsor_list = load_sponsors()
 
 host = conf['web']['host']
 port = conf['web']['port']
-# for Minecraft Java servers (needs to be enabled on the server)
-query_client = QueryClient(mc_host, port=mc_query)
 
-@ app.route('/assets/<path:filename>')
+
+@app.route('/assets/<path:filename>')
 def serve_static(filename):
     return send_from_directory('assets', filename)
 
-@ app.route('/images/<path:filename>')
+
+@app.route('/images/<path:filename>')
 def serve_images(filename):
     return send_from_directory('images', filename)
 
-@ app.route('/')
+
+@app.route('/')
 def home():
     offline = False
     try:
-        res = query_client.get_status()
+        response = requests.get(f'https://api.mcsrvstat.us/3/{mc_host}:{mc_port}').json()
+        offline = not response['online']
     except TimeoutError:
         offline = True
     if not offline:
-        cleaned_motd = re.sub(r'§[a-f0-9klmnor]', '', res.motd)
-        cleaned_motd = re.sub(r'[^a-zA-Z0-9\s]', '', cleaned_motd)
+        cleaned_motd = '\n'.join(response['motd']['clean'])
         player_list = []
-        for player in res.players.list:
-            try:
-                data = requests.get(f'https://playerdb.co/api/player/minecraft/{player}', timeout=5).json()
-                if data['success']:
-                    uuid = data['data']['player']['id']
-                    img = f'https://crafatar.com/renders/head/{uuid}'
-                else:
-                    img = 'https://crafatar.com/renders/head/aaaaaaaa-cf6b-4485-bef9-3957e7b7f509'
-            except requests.exceptions.ReadTimeout:
-                img = 'https://crafatar.com/renders/head/aaaaaaaa-cf6b-4485-bef9-3957e7b7f509'
-            player_list.append({'name': player, 'img': img})
+        for player in response['players']['list']:
+            name = player['name']
+            uuid = player['uuid']
+            img = f'https://crafatar.com/renders/head/{uuid}'
+            print(img)
+            player_list.append({'name': name, 'img': img})
         return render_template('index.html',
-                            name = mc_name,
-                            host = mc_host,
-                            port = mc_port,
-                            show_info = mc_show_info,
-                            motd = cleaned_motd,
-                            current = len(res.players.list),
-                            maxp = res.players.max,
-                            logo = mc_logo,
-                            preview_title = mc_preview_title,
-                            preview_descr = mc_preview_descr,
-                            preview_images = mc_preview_images,
-                            join_content = join_content,
-                            player_list = player_list,
-                            offline = offline)
+                               name=mc_name,
+                               host=mc_host,
+                               port=mc_port,
+                               show_info=mc_show_info,
+                               motd=cleaned_motd,
+                               current=response['players']['online'],
+                               maxp=response['players']['max'],
+                               logo=mc_logo,
+                               preview_title=mc_preview_title,
+                               preview_descr=mc_preview_descr,
+                               preview_images=mc_preview_images,
+                               player_list=player_list,
+                               sponsor_list=sponsor_list,
+                               offline=offline)
     else:
         return render_template('index.html',
-                            name = mc_name,
-                            host = mc_host,
-                            port = mc_port,
-                            show_info = mc_show_info,
-                            logo = mc_logo,
-                            preview_title = mc_preview_title,
-                            preview_descr = mc_preview_descr,
-                            preview_images = mc_preview_images,
-                            join_content = join_content,
-                            offline = offline)
-        
+                               name=mc_name,
+                               host=mc_host,
+                               port=mc_port,
+                               show_info=mc_show_info,
+                               logo=mc_logo,
+                               preview_title=mc_preview_title,
+                               preview_descr=mc_preview_descr,
+                               preview_images=mc_preview_images,
+                               offline=offline)
+
 
 if __name__ == '__main__':
     app.run(host, port)
---
+++ utils/sponsors.py
@@ -0,0 +1,34 @@
+import requests
+import yaml
+
+
+def load_sponsors() -> list:
+    with open("./sponsors.yml", "r",encoding='utf-8') as f:
+        data = yaml.safe_load(f)
+
+    with requests.Session() as session:
+
+        for e in data['sponsors']:
+            if 'player_name' not in e:
+                e['player_name'] = 'Unknown'
+            e['image'] = get_img(session, e['player_name'])
+            print(e)
+    return sorted(data['sponsors'],key=lambda x: x['amount'],reverse=True)
+
+
+def get_img(session, player_name) -> str:
+    if player_name == 'Unknown':
+        return 'https://crafatar.com/renders/head/aaaaaaaa-cf6b-4485-bef9-3957e7b7f509'
+    else:
+        with session.get(f'https://playerdb.co/api/player/minecraft/{player_name}') as data:
+            data = data.json()
+            if data['success']:
+                uuid = data['data']['player']['id']
+                img = f'https://crafatar.com/renders/head/{uuid}'
+            else:
+                img = 'https://crafatar.com/renders/head/aaaaaaaa-cf6b-4485-bef9-3957e7b7f509'
+            return img
+
+
+if __name__ == '__main__':
+    load_sponsors()
